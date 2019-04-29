3bc430c10a6c87d5273b48941169b3d2bcf0d211
Blake Rose <bdecktor@gmail.com>
2019-04-29 14:43:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(integar)
 counter=0
-while counter <countdown
-  puts "#{countdown} SECOND(S)!"
+while counter <integar
+  puts "#{integar} SECOND(S)!"
   counter-= 1
 end
   puts "HAPPY NEW YEAR!"
